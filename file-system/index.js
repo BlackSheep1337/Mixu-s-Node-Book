4f010dcfa09a88d0aaec45144495365d3ff25c12
Ale Pereira <naitwa-alexandre@gmail.com>
2025-01-07 10:16:34 -0300

    read dir example

--- file-system/index.js
+++ file-system/index.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 
-const fileData = 'data to the file';
-const arrayData = ['hello', 'world']
+// const fileData = 'data to the file';
+// const arrayData = ['hello', 'world']
 
 // const data = fs.readFileSync('./index.html', 'utf8');
 // console.log(data);
@@ -15,14 +15,37 @@ const arrayData = ['hello', 'world']
 //   console.log('File write completed');
 // });
 
-fs.open('./index.html', 'w', function (err, fd) {
+// fs.open('./index.html', 'w', function (err, fd) {
+//   if (err) throw err;
+//   const buff =  Buffer.from('bbbbbbb\n');
+//   fs.write(fd, buff, 0, buff.length, null, function (err, written, buffer) {
+//     if (err) throw err;
+//     console.log(err, written, buffer);
+//     fs.close(fd, function () {
+//       console.log('Done');
+//     });
+//   });
+// });
+
+// fs.open('./data/index.html', 'r', function (err, fd) {
+//   if (err) throw err;
+//   const buf = Buffer.from('3');
+//   fs.read(fd, buf, 0, buf.length, null, function (err, bytesRead, buffer) {
+//     if (err) throw err;
+//     console.log(err, bytesRead, buffer);
+//     fs.close(fd, function () {
+//       console.log('Done');
+//     });
+//   });
+// });
+
+const path = './data/';
+fs.readdir(path, function (err, files) {
   if (err) throw err;
-  const buff =  Buffer.from('bbbbbbb\n');
-  fs.write(fd, buff, 0, buff.length, null, function (err, written, buffer) {
-    if (err) throw err;
-    console.log(err, written, buffer);
-    fs.close(fd, function () {
-      console.log('Done');
-    });
+  files.forEach(function (file) {
+    console.log(path + file);
+    fs.stat(path + file, function (err, stats) {
+      console.log(stats);
+    })
   });
 });
